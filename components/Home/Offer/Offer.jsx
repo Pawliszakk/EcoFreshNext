a559e32271c651ee24c16e483c62d62dae769c7b
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-09-19 00:45:22 +0200

    added dividers, and decoration slide \\

---
+++ components/Home/Offer/Offer.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from '../../UI/SectionTitle';
+import classes from './Offer.module.scss';
+
+const Offer = () => {
+	return (
+		<section className={classes.offer} id="offer">
+			<SectionTitle>Oferta</SectionTitle>
+
+
+		</section>
+	);
+};
+
+export default Offer;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,15 @@
 import About from '../../components/Home/About/About';
+import Decoration from '../../components/Home/Decoration/Decoration';
 import Hero from '../../components/Home/Hero/Hero';
+import Offer from '../../components/Home/Offer/Offer';
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
 			<About />
+			<Decoration />
+			<Offer />
 		</>
 	);
 }
