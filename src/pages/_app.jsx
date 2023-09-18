3500b59d62f3e632baa14c5fda0b86e4d9f6ae08
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-09-18 22:55:46 +0200

    added favicons

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,29 @@
 import '@/styles/globals.css';
 import Layout from '../../components/layout/layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link
+					rel="icon"
+					href="/assets/icons/favicon.ico"
+					type="image/x-icon"
+					sizes="any"
+				/>
+				<link
+					rel="apple-touch-icon"
+					href="/assets/icons/apple-touch-icon.png"
+					type="image/png"
+					sizes="180x180"
+				/>
+				<link rel="shortcut icon" href="/assets/icons/favicon.ico" />
+			</Head>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
 	);
 }
