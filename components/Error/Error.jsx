c4e451e95a7307ec0120c624319c5c2feb33dc12
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-09-19 01:21:14 +0200

    added 404 page

--- components/Error/Error.jsx
+++ components/Error/Error.jsx
@@ -1,10 +1,12 @@
+import Link from 'next/link';
 import classes from './Error.module.scss';
 
 const Error = () => {
 	return (
 		<section className={classes.error}>
-			<h1>O nie!</h1>
+			<h1>Ups... błąd 404</h1>
 			<p>Strona której szukasz nie istnieje...</p>
+			<Link href="/">Wróć do strony głównej</Link>
 		</section>
 	);
 };
