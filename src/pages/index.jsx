41377bfc9043f010a6ec56aa69d20b03ac538552
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-09-20 17:57:30 +0200

    meta data

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import About from '../../components/Home/About/About';
 import Contact from '../../components/Home/Contact/Contact';
 import Decoration from '../../components/Home/Decoration/Decoration';
@@ -8,6 +9,13 @@ import Work from '../../components/Home/Work/Work';
 export default function Home() {
 	return (
 		<>
+			<Head>
+				<meta
+					name="description"
+					content="Eco Fresh Łabunie - Profesjonalna hurtownia owoców i warzyw! Zapewniamy świeże, zdrowe i wysokiej jakości warzywa i owoce. Zapraszamy!"
+				/>
+				<title>Hurtownia owoców i warzyw Eco Fresh Łabunie</title>
+			</Head>
 			<Hero />
 			<About />
 			<Decoration />
