dbef4fd77ad691a21ed1c1aaa8cb20882db23844
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-09-20 18:24:06 +0200

    pixels nav

--- components/Home/Contact/Contact.jsx
+++ components/Home/Contact/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
 						style={{ border: 0 }}
 						allowfullscreen=""
 						loading="lazy"
-						referrerpolicy="no-referrer-when-downgrade"
+						referrerPolicy="no-referrer-when-downgrade"
 					></iframe>
 				</SlideAnimation>
 				<div className={classes.icons}>
